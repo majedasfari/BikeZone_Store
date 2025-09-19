431327055a62216e58387302790607bcdddbb2cf
majedasfari <enjoyenglish46@gmail.com>
2025-09-19 13:10:44 +0300

    Add project files.

---
+++ BikeZone_Store/Data/ProjectoneDbcontext.cs
@@ -0,0 +1,18 @@
+﻿using BikeZone_Store.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BikeZone_Store.Data
+{
+
+    public class ProjectoneDbcontext : DbContext
+    {
+        public ProjectoneDbcontext(DbContextOptions<ProjectoneDbcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+
+
+}
